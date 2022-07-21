354df02f29f893e553c2167f07af28a9b2605aec
Abhishek Jha <69615011+Abhishekjha52@users.noreply.github.com>
2022-07-21 23:24:05 +0530

    Update DupliInArray.cpp

--- DupliInArray.cpp
+++ DupliInArray.cpp
@@ -1,5 +1,9 @@
 //Duplicate in an array
-vector<int> duplicates(int a[], int n) {
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<int> duplicates(int a[], int n) 
+{
         vector<int>ans;
         int freq[n];
         memset(freq,0,sizeof(freq));
@@ -14,3 +18,13 @@ vector<int> duplicates(int a[], int n) {
             ans.push_back(-1);
         return ans;
     }
+int main()
+{
+        int n;cin>>n;
+        vector<ll>a(n);
+        for(ll i=0;i<n;i++)cin>>a[i];
+        vector<ll>ans=duplicates(a, n);
+        for(auto x:ans)
+                cout<<x<<" ";
+        return 0;
+}
